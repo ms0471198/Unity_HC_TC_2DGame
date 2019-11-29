c209763a03e812b516fe13b517d160bc17fe2f32
KID <50666207+KID421@users.noreply.github.com>
2019-11-29 19:15:44 +0800

    使用 OnBecame 事件偵測水管在攝影機外面才執行刪除

--- UnityProject/Assets/Scripts/Pipe.cs
+++ UnityProject/Assets/Scripts/Pipe.cs
@@ -1,9 +1,17 @@
 ﻿// : 繼承 類別，可以享有繼承類別的成員
 public class Pipe : Ground
 {
-    private void Start()
+    // 掛腳本的物件要有 Mesh Renderer
+    // 在所有攝影機看不到時執行一次
+    private void OnBecameInvisible()
     {
         // 刪除(此遊戲物件，延遲 2.5 秒);
         Destroy(gameObject, 5f);
+    }
+
+    // 在所有攝影機看到時執行一次
+    private void OnBecameVisible()
+    {
+        
     }
 }
